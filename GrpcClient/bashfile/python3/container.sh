47892af308587b454f5871b77023c37860ef90f8
novel1025 <yeauptp4e6@gmail.com>
2023-02-06 09:40:17 +0900

    pythonのバッシュファイルに--rmをつけた

--- GrpcClient/bashfile/python3/container.sh
+++ GrpcClient/bashfile/python3/container.sh
@@ -1,1 +1,1 @@
-docker run -itd --privileged --name python3test -v /home/kondo/bashfile/python3/testData:/opt/testData -v /home/kondo/bashfile/python3/bin:/opt/bin/ python3 /sbin/init
+docker run -itd --privileged --rm --name python3test -v /home/kondo/bashfile/python3/testData:/opt/testData -v /home/kondo/bashfile/python3/bin:/opt/bin/ python3 /sbin/init
